8ec1f142ccf7c6c3c847e085f1a59a86dff7a863
JayGee <jayguarino@gmail.com>
2023-04-12 20:17:56 -0700

    mongoose added, model, route

--- app.js
+++ app.js
@@ -3,9 +3,21 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const registrantRouter = require('./routes/registrantRouter');
+
+
+//connect to mongoDB
+const url = 'mongodb://localhost:27017/bootcamp';
+const connect = mongoose.connect(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+connect.then(() => console.log('Connected correctly to the server'), err => console.log(err));
 
 var app = express();
 
@@ -21,6 +33,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('registrants', registrantRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
