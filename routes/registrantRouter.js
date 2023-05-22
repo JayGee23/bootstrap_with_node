0d3a9fbbcd54cce0077839a9fd1c9c73057cf15f
JayGee <jayguarino@gmail.com>
2023-05-22 15:56:32 -0700

    id endpoints added

--- routes/registrantRouter.js
+++ routes/registrantRouter.js
@@ -37,6 +37,42 @@ registrantRouter.route('/')
             .catch(err => next(err));
     });
 
+// registrants ID endpoints
+registrantRouter.route('/:registrantId')
+    .get((req, res, next) => {
+        Registrant.findById(req.params.registrantId)
+            .then(registrant => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(registrant);
+            })
+            .catch(err => next(err))
+    })
+    .post((req, res) => {
+        res.statusCode = 403;
+        res.end(`POST operation not supported on /registrant/${req.params.registrantId}`);
+    })
+    .put((req, res, next) => {
+        Registrant.findByIdAndUpdate(req.params.registrantId, {
+            $set: req.body
+        }, {
+            new: true
+        })
+            .then(registrant => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(registrant);
+            })
+            .catch(err => next(err));
+    })
+    .delete((req, res, next) => {
+        Registrant.findByIdAndDelete(req.params.registrantId)
+            .then(response => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/type');
+                res.json(response);
+            })
+    });
 
 //export
 module.exports = registrantRouter;
